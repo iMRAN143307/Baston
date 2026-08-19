90345037be6472a4626fe45898d6b3aafdc5967e
iMRAN143307 <imranmawji2@gmail.com>
2026-08-18 22:19:28 -0400

    concepts

--- baston.py
+++ baston.py
@@ -2,8 +2,12 @@ import pygame
 
 def create_move():
     pass
+    # list of shapes with position relative to the hurtbox, frames active and other info in sequence
 
 def create_character():
     pass
+    # create hurtbox(es) and add moves
 
+def collision_check():
+    pass
 #Have to add as many means of input handling as possible
