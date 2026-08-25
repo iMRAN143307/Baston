c0ac5f4eb6996841fd9ed42fd6ec3af9d87b9f95
iMRAN143307 <imranmawji2@gmail.com>
2026-08-24 20:11:37 -0400

    added new shapes

--- baston.py
+++ baston.py
@@ -9,6 +9,7 @@ hurtboxes_active = []
 #add tuples with ((x, y), player)
 
 def rectangle(x, y, w, h):
+    """Takes the top-left corner as (x, y)"""
     all_points = []
     for i in range(w):
         for j in range(h):
@@ -16,7 +17,13 @@ def rectangle(x, y, w, h):
     return all_points
 
 def circle(x, y, size):
-    pass
+    """Takes the centre as (x, y)"""
+    all_points = []
+    for i in range(size):
+        for j in range(size):
+            if (i**2 + j**2 <= size**2):
+                all_points.append((x + i, y + j))
+    return all_points
 
 def arch(x, y, w, left, right):
     pass
@@ -28,7 +35,13 @@ def vertical_pill(x, y, size):
     pass
 
 def triangle(x, y, size):
-    pass
+    """Takes the centre as (x, y)"""
+    all_points = []
+    for i in range(size):
+        for j in range(size):
+            if (j <= (i + size) * 1.73205081) and (j <= (i - size) * -1.73205081):
+                all_points.append((x + i, y + j))
+    return all_points
 
 def create_projectile():
     pass
@@ -36,7 +49,8 @@ def create_projectile():
 
 def create_move():
     pass
-    # list of shapes with position relative to the hurtbox, frames active and other info in sequence
+    # list of shapes with position relative to the hurtbox, frames active and other info in sequence or a create_projectile() function call
+    # info includes: knockback, knockback angle, hitstun, hitstop?, damage
 
 def create_character():
     pass
@@ -54,5 +68,15 @@ def collision_check():
     # check all hitboxes and hurtboxes for collision
     # lower all activity_frames_left by 1
     # if activity_frames_left == 0, delete the hitbox
+
+def zone_set():
+    pass
+
+    #set a special zone such as: blast zone bubble, death zone, special death zone, king of the hill zone, solid object zone
+
+def zone_collision_check():
+    pass
+
+    #check if any hurtboxes or hitboxes are in special zones
 
 # have to add as many means of input handling as possible
