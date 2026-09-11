85e22c024e233f5e15fc9de3e3d00c0c54bbe93b
iMRAN143307 <imranmawji2@gmail.com>
2026-09-10 21:20:09 -0400

    more hitbox/hurtbox handling done

--- baston.py
+++ baston.py
@@ -1,6 +1,8 @@
 import pygame
 from collections import defaultdict
 
+starting_lives = 1
+
 hitboxes_active = []
 
 # add lists with [(x, y), activity_frames_left, player, move]
@@ -66,6 +68,11 @@ def horizontal_pill(x, y, size):
 def vertical_pill(x, y, size):
     pass
 
+def apply_hurtboxes(points: list):
+    pass
+
+    # add all current hurtboxes here
+
 def create_projectile(hitbox, velocity, direction):
     """DO NOT CALL YOUR MOVE 'PROJECTILE'! IT IS BAD CODE DESIGN AND MAY LEAD TO ISSUES WITH BASTON'S HITBOX HANDLER"""
     for point in hitbox[0]:
@@ -79,11 +86,11 @@ def use_move(hitboxes: list):
 
     hitboxes look like: [shape(), frames_active, player, move]
 
-    moves look like: [[hitbox1, hitbox2], knockback, knockback angle, hitstun, hitstop?, damage, i-frames?, priority?]
+    moves look like: [[hitbox1, hitbox2], knockback, knockback angle, hitstun, damage, priority, i-frames?, hitstop?]
 
-    hybrid moves look like: [[hitbox1, ["projectile", hitbox2, velocity, direction]], knockback, knockback angle, hitstun, hitstop?, damage, i-frames?, priority?]
+    hybrid moves look like: [[hitbox1, ["projectile", hitbox2, velocity, direction]], knockback, knockback angle, hitstun, damage, priority, i-frames?, hitstop?]
 
-    projectile moves look like: [[["projectile", hitbox1, velocity, direction], ["projectile", hitbox2, velocity, direction]], knockback, knockback angle, hitstun, hitstop?, damage, i-frames?, priority?]
+    projectile moves look like: [[["projectile", hitbox1, velocity, direction], ["projectile", hitbox2, velocity, direction]], knockback, knockback angle, hitstun, damage, priority, i-frames?, hitstop?]
 
     """
     for hitbox in hitboxes:
@@ -96,31 +103,61 @@ def use_move(hitboxes: list):
 def create_character(hurtboxes: list):
     """
 
-    hurtboxes look like: ["move_name", (x_offset, y_offset), shape()]
+    hurtboxes look like: ["action", (x_offset, y_offset), shape()]
+
+    characters look like: [hurtboxes, damage, lives]
 
     """
 
-    char_hitboxes = defaultdict(list)
+    char_hurtboxes = defaultdict(list)
 
     for hurtbox in hurtboxes:
         for point in hurtbox[2]:
-            char_hitboxes[hurtbox[0]].append((point[0] + hurtbox[1][0], point[1] + hurtbox[1][1]))
+            char_hurtboxes[hurtbox[0]].append((point[0] + hurtbox[1][0], point[1] + hurtbox[1][1]))
 
-    return char_hitboxes
+    return [char_hurtboxes, 0, starting_lives]
 
     # create hurtbox(es) and add moves bound to inputs
     # hurtboxes are reusable by name
     # hurtbox x_offset and y_offset are from the top-left corner
 
-def hitbox_on_hitbox_collision():
-    pass
+def hitbox_on_hitbox_collision(hitbox1, hitbox2, move1, move2):
+    if move1[6] or move2[6] == 0:
+        pass
+        #transcendent priority
+    elif move1[6] > move2[6]:
+        to_remove = []
+        for hitbox in hitboxes_active:
+            if hitbox[1] == hitbox2[1] and hitbox[2] == hitbox2[2] and hitbox[3] == hitbox2[3]:
+                to_remove.append(hitbox)
+        for box in to_remove:
+            hitboxes_active.remove(box)
+    elif move1[6] < move2[6]:
+        to_remove = []
+        for hitbox in hitboxes_active:
+            if hitbox[1] == hitbox1[1] and hitbox[2] == hitbox1[2] and hitbox[3] == hitbox1[3]:
+                to_remove.append(hitbox)
+        for box in to_remove:
+            hitboxes_active.remove(box)
+    elif move1[6] == move2[6]:
+        to_remove = []
+        for hitbox in hitboxes_active:
+            if (hitbox[1] == hitbox1[1] and hitbox[2] == hitbox1[2] and hitbox[3] == hitbox1[3]) or (hitbox[1] == hitbox2[1] and hitbox[2] == hitbox2[2] and hitbox[3] == hitbox2[3]):
+                to_remove.append(hitbox)
+        for box in to_remove:
+            hitboxes_active.remove(box)
 
-    #refer back to the move for all the information
+    # pass in eval(hitbox[3])
 
-def hitbox_on_opposing_hurtbox_collision():
-    pass
+def hitbox_on_opposing_hurtbox_collision(hitbox, hurtbox, move):
+    to_remove = []
+    for box in hitboxes_active:
+        if hitbox[1] == box[1] and hitbox[2] == box[2] and hitbox[3] == box[3]:
+            to_remove.append(hitbox)
+    for box in to_remove:
+        hitboxes_active.remove(box)
 
-    #refer back to the move for all the information
+    # pass in eval(hitbox[3])
 
 def collision_check():
     pass
@@ -139,7 +176,11 @@ def zone_set(shape: list, type: str):
         zones_active.append([point, type])
 
 def zone_collision_check():
-    pass
+    for hitbox in hitboxes_active:
+        for zone in zones_active:
+            if hitbox[0] == zone[0]:
+                pass
+                #trigger collision based on type
 
     #check if any hurtboxes or hitboxes are in special zones
 
--- test.py
+++ test.py
@@ -5,6 +5,8 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 running = True
 
+starting_lives = 3
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
